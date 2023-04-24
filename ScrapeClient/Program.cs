a216c790584f96c5268fc26e707a571cc7e9219e
ayesukummi <ayesukummi@DESKTOP-VRC2LQO>
2023-04-24 21:02:58 +0100

    Add project files.

---
+++ ScrapeClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using ScrapeLibrary;
+
+namespace ScrapeClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Scrape myScrape = new();
+            string value = myScrape.ScrapeWebPage("https://learn.microsoft.com/");
+            Console.WriteLine(value);
+
+        }
+    }
+}
---
+++ ScrapeLibrary/Scrape.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+
+        public string ScrapeWebPage(string url)
+        {
+            return GetWebpage(url);
+
+        }
+        public string ScrapeWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+        private string GetWebpage(string url)
+        {
+            WebClient client = new();
+      
+            String content = client.DownloadString(url);
+            return content + "THATS ALL MY GUY"; 
+           
+        }
+    }
+}
